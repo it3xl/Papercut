93e88e4cad6876ab462e216ba94fbda129e995b6
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-09-27 16:28:35 +0300

    I will use the "yy.MMdd-HH.mm.ssfff" date time format for file names. It's much more readable.

--- src/Papercut.Core/Domain/Message/MessageEntry.cs
+++ src/Papercut.Core/Domain/Message/MessageEntry.cs
@@ -27,16 +27,16 @@ namespace Papercut.Core.Domain.Message
     using Papercut.Core.Annotations;
 
     /// <summary>
-    ///     The message entry.
+    /// The message entry.
     /// </summary>
     public class MessageEntry : INotifyPropertyChanged, IEquatable<MessageEntry>, IFile
     {
         // You can start by using another timestamp format. An example below.
-        //public const string DateTimeFormat = "yy.MMdd-HH.mm.ssfff";
+        public const string DateTimeFormat = "yy.MMdd-HH.mm.ssfff";
         // Bug. It is sucks to use FFF instead of fff.
         // I.e. for 170ms we will get the string 17 and we will be forced to use RegEx dirty coding.
         //public const string DateTimeFormat = "yyyyMMddHHmmssFFF";
-        public const string DateTimeFormat = "yyyyMMddHHmmssfff";
+        //public const string DateTimeFormat = "yyyyMMddHHmmssfff";
 
 
         //static readonly Regex _nameFormat = new Regex(
