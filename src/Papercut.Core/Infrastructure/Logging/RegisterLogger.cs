766737d702f13423cc9dc3600cc19f59b0270c15
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2018-10-15 18:47:33 +0300

    I want the Warning level for logging. Serilog > LoggerConfiguration > MinimumLevel > Warning https://github.com/serilog/serilog/wiki/Configuration-Basics

--- src/Papercut.Core/Infrastructure/Logging/RegisterLogger.cs
+++ src/Papercut.Core/Infrastructure/Logging/RegisterLogger.cs
@@ -59,7 +59,7 @@ namespace Papercut.Core.Infrastructure.Logging
 #if DEBUG
                                 .MinimumLevel.Verbose()
 #else
-                                .MinimumLevel.Information()
+                                .MinimumLevel.Warning()
 #endif
                                 .Enrich.With<EnvironmentEnricher>()
                                 .Enrich.FromLogContext()
